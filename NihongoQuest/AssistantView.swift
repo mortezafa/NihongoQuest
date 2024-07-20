87e64b0727795664091a8d41785c67a62830696d
MortezaFa <teezy10299@gmail.com>
2024-07-19 20:35:08 -0600

    added character tap logic

--- NihongoQuest/AssistantView.swift
+++ NihongoQuest/AssistantView.swift
@@ -8,6 +8,8 @@ struct AssistantView: View {
     @State var characterEntity: Entity = {
         let headAnchor = AnchorEntity(.head)
         headAnchor.position = [0.70, -0.35, -1]
+        let radians = -30 * Float.pi / 180 // converting -30 degrees to rads here
+        rotateEnityAboutY(entity: headAnchor, angle: radians)
         return headAnchor
 
     }()
@@ -25,7 +27,15 @@ struct AssistantView: View {
         }
     }
 
-    
+    static func rotateEnityAboutY(entity: Entity, angle: Float) {
+        var currentTransform = entity.transform
+
+        let rotation = simd_quatf(angle: angle, axis: [0,1,0])
+
+        currentTransform.rotation =  rotation * currentTransform.rotation
+
+        entity.transform = currentTransform
+    }
 }
 
 #Preview {
